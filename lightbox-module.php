3a3a419f0301f8070fa673f7f3a1210ee3d2ead3
Employee 451 <mail@askehippebrun.com>
2017-12-27 15:02:34 +0100

    Working edition
    Here, the Gallery shortcode works perfectly and the Vimeo shortcode has basic functionality. More to come in future updates!

--- lightbox-module.php
+++ lightbox-module.php
@@ -16,59 +16,66 @@ $lightbox_module_enabled = true;
  */
 function lightbox_module_scripts() {
   // Poptrox
-	wp_enqueue_script( 'strata-poptrox', plugins_url( 'assets/js/jquery.poptrox.min.js' ), array( 'jquery' ), null, true );
+	wp_enqueue_script( 'lightbox-module-poptrox', plugins_url( 'assets/js/jquery.poptrox.min.js', __FILE__ ), array( 'jquery' ), '2.5.2-dev', true );
 }
 add_action( 'wp_enqueue_scripts', 'lightbox_module_scripts' );
 
-/* Portfolio Markup */
-function customFormatGallery( $string, $attr ) {
-    $output = '<div class="row">';
-    $posts = get_posts( array( 'include' => $attr[ 'ids' ], 'post_type' => 'attachment' ) );
+function generate_lightbox_shortcodes( $text_domain, $container, $container_class, $from_indicator, $item_container, $item_class, $link_class, $title, $description ) {
+	// Gallery Shortcode
+	add_filter( 'post_gallery', function( $string, $attr ) use( $text_domain, $container, $container_class, $from_indicator, $item_container, $item_class, $link_class, $title, $description ) {
+		$output = '<' . $container . ( $container_class ? ' class="' . $container_class . '">' : '>' );
+		$posts = get_posts( array( 'include' => $attr[ 'ids' ], 'post_type' => 'attachment' ) );
 		$gallery_counter = 0;
 
-    foreach($posts as $imagePost) {
+		foreach($posts as $imagePost) {
 			$gallery_counter++;
 			$attachment = get_post( $imagePost->ID );
-			$mime_type = get_post_mime_type( $imagePost->ID );
-			if( strpos( $mime_type, 'video' ) !== false ) {
-				if( !has_post_thumbnail( $imagePost->ID ) )
-					continue;
 
-				$video_metadata = wp_get_attachment_metadata( $imagePost->ID );
-				$video_height = $video_metadata['height'];
-				$video_width = $video_metadata['width'];
-
-				$output .= '<article class="6u' . ( $gallery_counter % 2 === 0 ? '$' : '' ) . ' 12u$(xsmall) work-item">';
-				$output .= '<a href="' . $attachment->guid . '" class="image fit thumb" data-poptrox="iframe,' . $video_width . 'x' . $video_height . '">';
-				$output .= '<img src="' . get_the_post_thumbnail_url( $imagePost->ID, 'strata-thumb' ) . '" alt="' . $attachment->post_title . '" />';
-				$output .= '</a>';
-				$output .= '<h3>' . $attachment->post_title . '</h3>';
-				$output .= '<p>' . $attachment->post_content . '</p>';
-				$output .= '</article>';
+			if( $from_indicator ) {
+				$output .= '<' . $item_container . ' class="from-' . ( $gallery_counter % 2 === 0 ? 'right' : 'left' ) . ( $item_class ? ' ' . $item_class : '' ) . '">';
 			} else {
-				$output .= '<article class="6u' . ( $gallery_counter % 2 === 0 ? '$' : '' ) . ' 12u$(xsmall) work-item">';
-				$output .= '<a href="' . $attachment->guid . '" class="image fit thumb">';
-				$output .= '<img src="' . wp_get_attachment_image_src( $imagePost->ID, 'strata-thumb' )[0] . '" alt="' . $attachment->post_title . '" />';
-				$output .= '</a>';
-				$output .= '<h3>' . $attachment->post_title . '</h3>';
-				$output .= '<p>' . $attachment->post_content . '</p>';
-				$output .= '</article>';
+				$output .= '<' . $item_container . ' class="6u' . ( $gallery_counter % 2 === 0 ? '$' : '' ) . ' 12u$(xsmall)' . ( $item_class ? ' ' . $item_class : '' ) . '">';
 			}
-    }
 
-    $output .= "</div>";
+			$output .= '<a href="' . $attachment->guid . '"' . ( $link_class ? ' class="' . $link_class . '">' : '>' );
+			$output .= '<img src="' . wp_get_attachment_image_src( $imagePost->ID, $text_domain . '-thumb' )[0] . '" alt="' . $attachment->post_title . '" />';
+			$output .= '</a>';
 
-    return $output;
-}
-add_filter( 'post_gallery', 'customFormatGallery', 10, 2 );
+			if( $title ) {
+				$output .= '<h3>' . $attachment->post_title . '</h3>';
+			}
 
-/* Vimeo Shortcode */
-function strata_vimeo_shortcode( $atts ) {
-    $a = shortcode_atts( array(
-        'ids' => '',
-    ), $atts );
+			if( $description ) {
+				$output .= '<p>' . $attachment->post_content . '</p>';
+			}
 
-		$output = '<div class="row">';
+			$output .= '</' . $item_container . '>';
+		}
+
+		$output .= '</' . $container . '>';
+
+		return $output;
+	}, 10, 2 );
+
+	/* Playlist shortcode */
+	// if( strpos( $mime_type, 'video' ) !== false ) {
+	// 	if( !has_post_thumbnail( $imagePost->ID ) )
+	// 		continue;
+  //
+	// 	$video_metadata = wp_get_attachment_metadata( $imagePost->ID );
+	// 	$video_height = $video_metadata['height'];
+	// 	$video_width = $video_metadata['width'];
+  //
+	// 	$output .= '<a href="' . $attachment->guid . '" ' . ( $link_class ? 'class="' . $link_class . '" ' : '' ) . 'data-poptrox="iframe,' . $video_width . 'x' . $video_height . '">';
+	// }
+
+	// Vimeo Shortcode
+	add_shortcode( 'vimeo_gallery', function( $atts ) use( $text_domain, $container, $container_class, $from_indicator, $item_container, $item_class, $link_class, $title, $description ) {
+		$a = shortcode_atts( array(
+				'ids' => '',
+		), $atts );
+
+		$output = '<' . $container . ( $container_class ? ' class="' . $container_class . '">' : '>' );
 		$gallery_counter = 0;
 		$ids = explode( ',', $a['ids'] );
 
@@ -77,24 +84,35 @@ function strata_vimeo_shortcode( $atts ) {
 			$trimmed_id = trim( $id );
 			$hash = json_decode(file_get_contents('http://vimeo.com/api/v2/video/' . $trimmed_id . '.json'));
 
-			$output .= '<article class="6u' . ( $gallery_counter % 2 === 0 ? '$' : '' ) . ' 12u$(xsmall) work-item">';
-			$output .= '<a href="' . $hash[0]->url . '" data-poptrox="vimeo,800x480" class="image fit thumb">';
+			if( $from_indicator ) {
+				$output .= '<' . $item_container . ' class="from-' . ( $gallery_counter % 2 === 0 ? 'right' : 'left' ) . ( $item_class ? ' ' . $item_class : '' ) . '">';
+			} else {
+				$output .= '<' . $item_container . ' class="6u' . ( $gallery_counter % 2 === 0 ? '$' : '' ) . ' 12u$(xsmall)' . ( $item_class ? ' ' . $item_class : '' ) . '">';
+			}
+
+			$output .= '<a href="' . $hash[0]->url . '" data-poptrox="vimeo,800x480"' . ( $link_class ? ' class="' . $link_class . '">' : '>' );
 			$output .= '<img src="' . $hash[0]->thumbnail_large . '" alt="' . $hash[0]->title . '" />';
 			$output .= '</a>';
-			$output .= '<h3>' . $hash[0]->title . '</h3>';
-			$output .= '<p>' . str_replace(array("<br>", "<br/>", "<br />"), NULL, $hash[0]->description) . '</p>';
-			$output .= '</article>';
+
+			if( $title ) {
+				$output .= '<h3>' . $hash[0]->title . '</h3>';
+			}
+
+			if( $description ) {
+				$output .= '<p>' . str_replace(array("<br>", "<br/>", "<br />"), NULL, $hash[0]->description) . '</p>';
+			}
+
+			$output .= '</' . $item_container . '>';
 		}
 
-		$output .= "</div>";
+		$output .= '</' . $container . '>';
 
-    return $output;
+		return $output;
+	} );
 }
-add_shortcode( 'vimeo_gallery', 'strata_vimeo_shortcode' );
 
 /** To-do:
- * Add classes to the pages that include the shortcodes
- * Add JS for activating poptrox on said pages ^
- * Default design for themes that don't naturally have lightbox implementation
- * More shortcodes! (YouTube, Wistia, Brightcove, Soundcloud, IFRAME, (AJAX), )
+ * Only giving user ID in Vimeo Shortcode, playlist?
+ * More shortcodes! (Playlist, YouTube, Wistia, Brightcove, Soundcloud, IFRAME, (AJAX), )
+ * Default design & functionality for themes that don't naturally have lightbox implementation (JS for activating poptrox, add class to pages that include shortcodes)
  */
